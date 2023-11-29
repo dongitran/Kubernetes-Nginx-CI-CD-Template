6592a1cf3dde24cbd5b769c813577c8d50cea109
dongtran <thiendong.iuh@gmail.com>
2023-11-29 22:39:31 +0700

    update package.json

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,11 +1,14 @@
-const express = require('express');
-const app = express();
+const http = require('http');
 
-app.get('/', (req, res) => {
-  res.send('Hello from Node.js server using Express!');
+const hostname = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
